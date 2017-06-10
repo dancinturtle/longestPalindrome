b8c4ebcc092cc54ee26e735943771e62a1dd8062
chosenman <chosenman2008@gmail.com>
2017-06-10 00:09:05 -0700

    added function cleaner with default range of lowercase alphabet

--- polyndrome.js
+++ polyndrome.js
@@ -5,3 +5,15 @@ function reverse(str){
 	}
 	return reversed
 }
+
+function cleaner(str, min, max){
+	var cleanString = "";
+	var min = min || 97;
+	var max = max || 122;
+	for (var i = 0; i < str.length; i++) {
+		if(str.charCodeAt(i) >= min && str.charCodeAt(i) <= max){
+			cleanString += str[i];
+		}
+	}
+	return cleanString;
+}
